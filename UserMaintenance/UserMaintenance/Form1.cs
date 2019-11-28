77315d68623243f6be7f83f039ebcc03551af1ac
Ábel Kováts <abel.kovats@stud.uni-corvinus.hu>
2019-11-28 14:39:44 +0100

    Part II

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -12,6 +12,8 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<Entities.User> users = new BindingList<Entities.User>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,20 @@ namespace UserMaintenance
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
 
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new Entities.User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+
+            users.Add(u);
         }
     }
 }
